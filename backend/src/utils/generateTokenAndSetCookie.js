127f1c238558bfbf8152fe8eee2d202eeef59cb5
Shivam Kumar <142471115+shivam108ku@users.noreply.github.com>
2025-07-25 12:57:46 +0530

    Update generateTokenAndSetCookie.js

--- backend/src/utils/generateTokenAndSetCookie.js
+++ backend/src/utils/generateTokenAndSetCookie.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = (res,userId,role)=>{
     res.cookie("token",token,{
         httpOnly:true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'None',
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
     return token;
