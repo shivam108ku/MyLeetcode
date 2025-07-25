dedf9477881946743f6b91e7c1ae1338fb0c32c1
Shivam Kumar <shivam108ku@gmail.com>
2025-07-25 16:58:25 +0530

    .

--- backend/src/utils/generateTokenAndSetCookie.js
+++ backend/src/utils/generateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ const generateTokenAndSetCookie = (res,userId,role)=>{
 
     res.cookie("token",token,{
         httpOnly:true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         sameSite: 'strict',
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
@@ -15,3 +15,26 @@ const generateTokenAndSetCookie = (res,userId,role)=>{
 }
 
 module.exports = generateTokenAndSetCookie;
+
+
+// const jwt = require('jsonwebtoken');
+
+// const generateTokenAndSetCookie = (res, userId, role) => {
+//     const token = jwt.sign({ userId, role }, process.env.JWT_SECRET, {
+//         expiresIn: '7d',
+//     });
+
+//     res.cookie("token", token, {
+//         httpOnly: true,
+//         secure: true,            // must be true on HTTPS
+//         sameSite: 'None',        // must be 'None' for cross-site cookie
+//         maxAge: 7 * 24 * 60 * 60 * 1000,
+//     });
+
+//     return token;
+// };
+
+// module.exports = generateTokenAndSetCookie;
+
+
+
